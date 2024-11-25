06ce0311b9d7ef054131b1cfe2f525876911e569
kenmy <kenmy@mtsocialdao.com>
2024-11-25 20:59:46 +0800

    update

--- cmd/meowlnir/eventhandling.go
+++ cmd/meowlnir/eventhandling.go
@@ -161,13 +161,15 @@ func (m *Meowlnir) HandleEncrypted(ctx context.Context, evt *event.Event) {
 		fmt.Println("不为空")
 		cryptohelper := CryptoHelperByBotUsername(ctx, m.EventProcessor, m.AS, m.CryptoStoreDB, evt.ToUserID, m.Config.Meowlnir.PickleKey, m.HandleMessage)
 		//	HandleEncrypted(ctx, cryptohelper, evt)
-		cryptohelper.HandleEncrypted(ctx, evt)
+		//cryptohelper.HandleEncrypted(ctx, evt)
+		botClient.CryptoHelper.HandleEncrypted(ctx, evt)
 		_ = cryptohelper
 	} else {
 		fmt.Println("toUserId为空")
 		cryptohelper := CryptoHelperByBotUsername(ctx, m.EventProcessor, m.AS, m.CryptoStoreDB, id.NewUserID("meowlnir002_bot", "server.mtsocialdao.com"), m.Config.Meowlnir.PickleKey, m.HandleMessage)
 		//HandleEncrypted(ctx, cryptohelper, evt)
 		cryptohelper.HandleEncrypted(ctx, evt)
+		botClient.CryptoHelper.HandleEncrypted(ctx, evt)
 		_ = cryptohelper
 
 	}
